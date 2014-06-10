ffb89198c6d76236985cb3403fa0cc0f2be587cd
David Huerta <david@haystackproject.com>
2014-06-09 23:09:10 -0400

    Window centering prolly.

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 chrome.app.runtime.onLaunched.addListener(function() {
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
   chrome.app.window.create('search.htm', {
     'bounds': {
-      'width': 400,
-      'height': 500
+      'width': width,
+      'height': height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
     }
   });
 });
